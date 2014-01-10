5c132b58023230de1cba76da6f90c4d908bfc6fa
batteryS <ez21cegi@gmail.com>
2014-01-10 14:23:54 +0900

    [tracker] Changed option menu for monitoring modes

--- src/com/lge/ccdevs/tracker/CameraActivity.java
+++ src/com/lge/ccdevs/tracker/CameraActivity.java
@@ -396,7 +396,9 @@ public class CameraActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         menu.add(0, ID_SET_TARGET, 0, "set target");
-        menu.add(0, ID_SET_BOUNDARY, 0, "set boundary");
+        if( mMonitorMode==MonitorModeActivity.MONITOR_MODE_PET ) {
+            menu.add(0, ID_SET_BOUNDARY, 0, "set boundary");
+        }
         menu.add(0, ID_START_RECORD, 0, "start recording");
         menu.add(0, ID_STOP_RECORD, 0, "stop recording");
         return super.onCreateOptionsMenu(menu);
